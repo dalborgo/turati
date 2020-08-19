9a32b9fce81c829596338a9cd952aded975f3185
dalborgo <marco@dalborgo.it>
2020-08-19 13:00:30 +0200

    second

--- src/index.js
+++ src/index.js
@@ -5,17 +5,22 @@ import Q from 'q'
 import path from 'path'
 import chalk from 'chalk'
 
-const destDir = 'e:\\Dati'
+const promises = []
 
-async function main (baseDir, limitMonths, prod = true) {
-  const splitFolder = baseDir.split('\\')
-  const secFolder = splitFolder[splitFolder.length - 1]
+const destDir = 'e:\\Dati'
+const fromBaseDir = 'c:\\Users\\Asten\\Documents'
+promises.push(() => main('INVENTARI', 6, false))
+promises.push(() => main('TXT\\SUMITOMO', 12, false))
+
+async function main (secFolder, limitMonths, prod = true) {
+  let count = 0
+  const baseDir = path.join(fromBaseDir, secFolder)
   console.info('Folder to copy:', chalk.blue(secFolder))
   console.log()
   try {
     const files = await readdir(baseDir)
     for await (const file of files) {
-      const stat = await Q.ninvoke(fs, 'stat', file)
+      const stat = await Q.nfcall(fs.stat, file)
       const fromTime = new Date(stat.mtime)
       const date = moment(fromTime)
       const now = moment()
@@ -33,37 +38,44 @@ async function main (baseDir, limitMonths, prod = true) {
         console.log('dest:', dest)
         if (!fs.existsSync(dest)) {
           const dirExists = fs.existsSync(destRest)
-          !dirExists && await Q.nfcall(fs.mkdir, destRest)
-          await Q.ninvoke(fs, 'copyFile', file, dest)
+          try {
+            !dirExists && await Q.nfcall(fs.mkdir, destRest, { recursive: true })
+            await Q.nfcall(fs.copyFile, file, dest)
+          } catch (err) {
+            console.warn(chalk.yellow(err.message))
+            continue
+          }
           if (prod) {
             try {
-              await Q.ninvoke(fs, 'unlink', file)
+              await Q.nfcall(fs.unlink, file)
             } catch (err) {
-              console.warn(err.message)
+              console.warn(chalk.yellow(err.message))
               console.log()
               continue
             }
           }
+          count++
           console.log(chalk.green('OK'))
+        } else {
+          console.log(chalk.green('SKIPPED'))
         }
         console.log()
       }
     }
+    return { ok: true, taskName: secFolder, count }
   } catch (err) {
-    console.error(err.message)
-    return { ok: false, message: err.message, err }
+    console.error(chalk.red(err.message))
+    return { ok: false, message: err.message, err, taskName: secFolder }
   }
 }
-
-const promises = []
-
-promises.push(() => main('c:\\tmp', 2)) //\\10.0.0.17\Asten\MARCO\prova
-//promises.push(() => main('\\\\10.0.0.17\\Asten\\MARCO\\prova2', 2))
 
 async function execAll () {
+  const response = []
   for (let exec of promises) {
-    await exec()
+    response.push(await exec())
   }
+  console.log()
+  return { response }
 }
 
-execAll().then()
+execAll().then(res => console.log(JSON.stringify(res, null, 2)))
